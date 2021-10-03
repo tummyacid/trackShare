cc2b23bdeebf2d3ab8aca4c2bd44a5fa186e4f70
tummyacid <tummyacid@gmail.com>
2021-10-03 14:59:22 -0700

    fixed bcrypt ref

--- files-upload/auth.js
+++ files-upload/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require("http");
 const bodyParser = require('body-parser');
 
-const bcryptjs = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Pool, Client } = require('pg')
 
